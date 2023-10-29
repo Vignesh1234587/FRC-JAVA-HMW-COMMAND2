43662d7b9e301c02195e3e38db6577fe952165f5
Vignesh1234587 <vigneshprabhakaran05@gmail.com>
2023-10-29 16:58:06 -0400

    added solutuion

--- commandSolutions/src/main/java/frc/robot/RobotContainer.java
+++ commandSolutions/src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,13 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.drive;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -20,35 +25,26 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
+  public final static DriveTrain drivetrain = new DriveTrain();
+  private final static Joystick joy = new Joystick(0);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    //Alternate option 
+    //drivetrain.setDefaultCommand(new RunCommand(() -> drivetrain.drive(getLeftJoyY(), getRightJoyX()), drivetrain));
+ 
+   drivetrain.setDefaultCommand(new drive());
+
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
+  
+  
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+   
+    new Trigger(() -> joy.getRawButton(1)).onTrue(new InstantCommand(drivetrain::resetGyro, drivetrain));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -58,6 +54,15 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
+  }
+
+  public static double getLeftJoyY(){
+
+    return joy.getRawAxis(0);
+  }
+  public static double getRightJoyX(){
+
+    return joy.getRawAxis(4);
   }
 }
--- commandSolutions/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ commandSolutions/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,17 +4,47 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.Pigeon2;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
   
+  TalonFX leftBack;
+  TalonFX rightBack;
+  TalonFX leftFront;
+  TalonFX rightFront;
+
+  Pigeon2 pigeon;
   public DriveTrain() {
+    leftBack = new TalonFX(0);
+    rightBack = new TalonFX(1);
+    leftFront = new TalonFX(2);
+    rightFront= new TalonFX(3);
+    
 
-
+    leftBack.follow(leftFront);
+    rightBack.follow(rightFront);
+    pigeon = new Pigeon2(6);
 
   }
 
+
+
+  public void drive(double x, double y){
+    
+    rightFront.set(ControlMode.PercentOutput, x);
+    leftFront.set(ControlMode.PercentOutput, y);
+
+  }
+
+  public void resetGyro(){
+    pigeon.setYaw(0);
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
